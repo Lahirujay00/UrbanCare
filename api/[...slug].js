bb41e8ae02cb16347e0a8f60d34dfdc1500deb70
Lahirujay00 <lahirutharaka02@gmail.com>
2025-11-25 12:15:00 +0530

    Improve API catch-all: Add async handler and logging

--- api/[...slug].js
+++ api/[...slug].js
@@ -1,2 +1,10 @@
-// Catch-all proxy to the main serverless function
-module.exports = require('../server/api/index.js');
+// Catch-all API route that proxies to Express app
+const handler = require('../server/api/index.js');
+
+module.exports = async (req, res) => {
+  // Log incoming request for debugging
+  console.log(`🔵 API Request: ${req.method} ${req.url}`);
+  
+  // Pass to main handler
+  return handler(req, res);
+};
