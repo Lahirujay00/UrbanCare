be2713fed10207c7069ca11c16bc2b5a4043a8c5
Lahirujay00 <lahirutharaka02@gmail.com>
2025-11-25 14:30:00 +0530

    Add root index.js to satisfy Vercel entrypoint check

---
+++ index.js
@@ -0,0 +1,5 @@
+// This file exists only to satisfy Vercel's entrypoint check
+// The actual serverless functions are in /api folder
+module.exports = () => {
+  console.log('This entry point is not used - serverless functions in /api handle all requests');
+};
